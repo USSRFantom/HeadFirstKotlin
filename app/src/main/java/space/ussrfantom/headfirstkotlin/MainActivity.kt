41d30259b44bcc7198f5a3b2be9bcb34004d203c
UssrFantom <dimkakrohin@mail.ru>
2020-12-14 19:53:47 +0300

    Lesson One finish

--- app/src/main/java/space/ussrfantom/headfirstkotlin/MainActivity.kt
+++ app/src/main/java/space/ussrfantom/headfirstkotlin/MainActivity.kt
@@ -10,10 +10,56 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         helloWorld()
+        lessonOne()
 
     }
 
     private fun helloWorld (){
         println("HelloWorld")
+    }
+
+    private fun lessonOne(){
+        //команды
+        var x = 3
+        var y = 2
+        val name = "Cormoran"
+        x = x * 10
+        println("x is $x.")
+
+        //циклы
+        while (x > 20){
+            x = x - 1
+            println("x is $x.")
+        }
+
+        for (i in 1..10){
+            x = x + 1
+            println("x is $x.")
+        }
+
+        //Если
+        if (x == 20){
+            println("x must be 20.")
+        }else{
+            println("x isn't 20.")
+        }
+
+        if(name.equals("Cormoran")){
+            println("$name Strike")
+        }
+
+        println(if (x > y) "пробуем упрощенный вид" else "не пробуем упрощенный вид")
+
+        //развлечение с магнитом
+
+        var i = 1
+        while (i < 3){
+            println(if(i == 1) "Yab" else "Dab")
+            print("ba")
+            i = i + 1
+
+        }
+        if (i == 3) print("Do")
+
     }
 }
